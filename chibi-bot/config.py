b5a363523ba8f594d227c98e5c4ea6f0f7d22e3d
therootishere00-ux <theroot.ishere00@gmail.com>
2025-10-30 17:29:24 +0300

    Create config.py

---
+++ chibi-bot/config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+# Текст для сообщений
+START_STICKER_ID = "CAACAgIAAxkBAAE9JsNpAzQZv6b4b-KZ3ftL2Sld0kUjDQAC400AAkuWEEosjitzZk8fzDYE"
+
+# Конфигурация
+POLLING_INTERVAL = 1.0
+TIMEOUT = 10
